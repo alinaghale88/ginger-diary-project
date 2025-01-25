0408be7b08ef2a9366f1dac83e2d0fbdafbc2ea8
alinaghale88 <alinaghale.official@gmail.com>
2025-01-25 02:36:02 -0800

    Set up Express server with MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
@@ -9,8 +10,7 @@ app.get("/entries", (req, res) => {
 
 });
 
-console.log(process.env.MONGO_URI);
-
 app.listen(5000, () => {
+    connectDB();
     console.log("Server started at http://localhost:5000");
 });
