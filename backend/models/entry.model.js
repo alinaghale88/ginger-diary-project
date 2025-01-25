840cc76894a671f46bd5801fdc26f02f262473a2
alinaghale88 <alinaghale.official@gmail.com>
2025-01-25 09:59:25 -0800

    Define Entry model with schema

---
+++ backend/models/entry.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const entrySchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    content:{
+        type:String,
+        required:true
+    },
+}, {
+    timestamps: true // createdAt, updatedAt
+});
+
+const Entry = mongoose.model('Entry', entrySchema);
+
+export default Entry;
