2ae6b9f4f2f7a10e198b87d8a530b40976991c95
alinaghale88 <alinaghale.official@gmail.com>
2025-01-25 10:38:44 -0800

    Add endpoint to create a new journal entry

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Entry from "./models/entry.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/entries", (req, res) => {
+app.post("/entries", async (req, res) => {
+    const entry = req.body; // user will send this data
 
+    if(!entry.title || !entry.content) {
+        return res.status(400).json({ success: false, message: "Please enter all fields" })
+    }
+
+    const newEntry = new Entry(entry);
+
+    try {
+        await newEntry.save();
+        res.status(201).json({ success: true, data: newEntry }); // Status 201: Something created
+    } catch (error) {
+        console.error("Error in Create entry:", error.message);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
 });
 
 app.listen(5000, () => {
