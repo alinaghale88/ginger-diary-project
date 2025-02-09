a2f2a7c3e13252e80fb613e701d7a95388496e2d
alinaghale88 <alinaghale.official@gmail.com>
2025-02-09 11:03:14 -0800

    Register user authentication routes in server.js

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,9 @@
+// login user
+export const loginUser = async (req, res) => {
+    res.json({ mssg: 'login user' })
+}
+
+// signup user
+export const signupUser = async (req, res) => {
+    res.json({ mssg: 'signup user' })
+}
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 
 import entryRoutes from "./routes/entry.route.js";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -11,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
+// routes
 app.use("/api/entries", entryRoutes);
+app.use("/api/user", userRoutes);
 
 app.listen(5000, () => {
     connectDB();
