9b1b19d4a32832e1dfc39f1802b129baca781cd7
alinaghale88 <alinaghale.official@gmail.com>
2025-02-09 14:51:46 -0800

    Add email & password validation in signup method

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import validator from "validator";
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -15,6 +16,18 @@ const userSchema = new mongoose.Schema({
 
 // static signup method
 userSchema.statics.signup = async function (email, password) {
+
+    // validation
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Email is not valid')
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough')
+    }
+
     const exists = await this.findOne({ email })
 
     if (exists) {
