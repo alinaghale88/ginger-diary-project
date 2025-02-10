00c0055b1c195eef47910b4ab4efae84ba982ad3
alinaghale88 <alinaghale.official@gmail.com>
2025-02-10 03:44:08 -0800

    JWT token generation and response with token

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,9 @@
-import User from "../models/user.model.js";
+import User from "../models/user.model.js"
+import jwt from "jsonwebtoken"
+
+const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '1d' })
+}
 
 // login user
 export const loginUser = async (req, res) => {
@@ -12,7 +17,10 @@ export const signupUser = async (req, res) => {
     try {
         const user = await User.signup(email, password)
 
-        res.status(200).json({ email, user })
+        // create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({ email, token })
     } catch (error) {
         res.status(400).json({ error: error.message })
     }
