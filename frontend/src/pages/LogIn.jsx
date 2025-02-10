bfbdd44eb53c741507d41f14b3aff06c07e60fd7
alinaghale88 <alinaghale.official@gmail.com>
2025-02-10 12:55:51 -0800

    Add login logic with context and localStorage

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
+import { useLogin } from "@/hooks/useLogin";
 
-const LogIn = () => {
+const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const { login, error, isLoading } = useLogin()
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        console.log(email, password)
+        await login(email, password)
     }
 
     return (
@@ -20,9 +22,10 @@ const LogIn = () => {
             <label>Password:</label>
             <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
 
-            <button>Log In</button>
+            <button disabled={isLoading}>Log In</button>
+            {error & <div className="error">{error}</div>}
         </form>
     )
 }
 
-export default LogIn
+export default Login
