1c4e330ee5c94ead3695bd63794c49796e612b76
alinaghale88 <alinaghale.official@gmail.com>
2025-02-10 17:24:12 -0800

    Redirect users based on authentication status

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,26 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import './App.css'
 import Dashboard from './pages/Dashboard'
-import LogIn from './pages/LogIn'
+import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 import JournalPage from './pages/JournalPage'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
+import { useAuthContext } from './hooks/useAuthContext'
 
 const App = () => {
+  const { user } = useAuthContext()
+
   return (
     <div className='flex'>
       <Navbar className='z-50' />
       <div className='-ml-7 w-full'>
         <Header />
         <Routes>
-          <Route path="/" element={<Dashboard />}></Route>
-          <Route path="/login" element={<LogIn />}></Route>
-          <Route path="/signup" element={<SignUp />}></Route>
-          <Route path="/journal" element={<JournalPage />}></Route>
+          <Route path="/" element={user ? <Dashboard /> : <Navigate to="/login" />}></Route>
+          <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />}></Route>
+          <Route path="/signup" element={!user ? <SignUp /> : <Navigate to="/" />}></Route>
+          <Route path="/journal" element={user ? <JournalPage /> : <Navigate to="/login" />}></Route>
         </Routes>
       </div>
     </div>
